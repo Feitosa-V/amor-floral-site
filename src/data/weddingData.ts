7121bf428cf065abc1dcbf7124f8cea949029074
Vinícius Feitosa <vifesi4321@gmail.com>
2025-10-13 15:37:53 -0300

    adicionado os presentes

--- src/data/weddingData.ts
+++ src/data/weddingData.ts
@@ -34,33 +34,298 @@ export const weddingData = {
       },
       {
         id: "2",
-        image: "public/images/gifts/gift_02.png",
+        image: "public/images/gifts/gift_02_certo.jpg",
         productUrl: "https://mpago.la/1eeTfvq"
       },
       {
         id: "3",
-        image: "public/images/gifts/gift_03.png",
+        image: "public/images/gifts/gift_03_certo.jpg",
         productUrl: "https://mpago.la/2dq2d7i"
       },
       {
         id: "4",
-        image: "public/images/gifts/gift_04.png",
+        image: "public/images/gifts/gift_04_certo.jpg",
         productUrl: "https://mpago.la/1uMPqfW"
       },
       {
         id: "5",
-        image: "public/images/gifts/gift_05.png",
+        image: "public/images/gifts/gift_05_certo.jpg",
         productUrl: "https://mpago.la/1FBzDPL"
       },
       {
         id: "6",
-        image: "public/images/gifts/gift_06.png",
+        image: "public/images/gifts/gift_06_certo.jpg",
         productUrl: "https://mpago.la/1ZLuYgt"
       },
       {
         id: "7",
-        image: "public/images/gifts/gift_07.png",
-        productUrl: "https://www.exemplo.com/produto7"
+        image: "public/images/gifts/gift_07_certo.jpg",
+        productUrl: "https://mpago.la/2btyETS"
+      },
+      {
+        id: "8",
+        image: "public/images/gifts/gift_08.jpg",
+        productUrl: "https://mpago.la/1PP4Ybw"
+      },
+      {
+        id: "9",
+        image: "public/images/gifts/gift_09.jpg",
+        productUrl: "https://mpago.la/2gUBRJH"
+      },
+      {
+        id: "10",
+        image: "public/images/gifts/gift_10.jpg",
+        productUrl: "https://mpago.la/2DZ4SeE"
+      },
+      {
+        id: "11",
+        image: "public/images/gifts/gift_11.jpg",
+        productUrl: "https://mpago.la/1eHTcBo"
+      },
+      {
+        id: "12",
+        image: "public/images/gifts/gift_12.jpg",
+        productUrl: "https://mpago.la/1GtAbKA"
+      },
+      {
+        id: "13",
+        image: "public/images/gifts/gift_13.jpg",
+        productUrl: "https://mpago.la/1XptZDC"
+      },
+      {
+        id: "14",
+        image: "public/images/gifts/gift_14.jpg",
+        productUrl: "https://mpago.la/114QVAb"
+      },
+      {
+        id: "15",
+        image: "public/images/gifts/gift_15.jpg",
+        productUrl: "https://mpago.la/2Zq6fsg"
+      },
+      {
+        id: "16",
+        image: "public/images/gifts/gift_16.jpg",
+        productUrl: "https://mpago.la/1Yjmg5d"
+      },
+      {
+        id: "17",
+        image: "public/images/gifts/gift_17.jpg",
+        productUrl: "https://mpago.la/16Ff9VC"
+      },
+      {
+        id: "18",
+        image: "public/images/gifts/gift_18.jpg",
+        productUrl: "https://mpago.la/18NyfMC"
+      },
+      {
+        id: "19",
+        image: "public/images/gifts/gift_19.jpg",
+        productUrl: "https://mpago.la/1TANs1Q"
+      },
+      {
+        id: "20",
+        image: "public/images/gifts/gift_20.jpg",
+        productUrl: "https://mpago.la/1djJYBK"
+      },
+      {
+        id: "21",
+        image: "public/images/gifts/gift_21.jpg",
+        productUrl: "https://mpago.la/2ydxGgW"
+      },
+      {
+        id: "22",
+        image: "public/images/gifts/gift_22.jpg",
+        productUrl: "https://mpago.la/1aBSRMG"
+      },
+      {
+        id: "23",
+        image: "public/images/gifts/gift_23.jpg",
+        productUrl: "https://mpago.la/1aBSRMG"
+      },
+      {
+        id: "24",
+        image: "public/images/gifts/gift_24.jpg",
+        productUrl: "https://mpago.la/19WCobW"
+      },
+      {
+        id: "25",
+        image: "public/images/gifts/gift_25.jpg",
+        productUrl: "https://mpago.la/1B7Xdbe"
+      },
+      {
+        id: "26",
+        image: "public/images/gifts/gift_26.jpg",
+        productUrl: "https://mpago.la/2Q2KXxU"
+      },
+      {
+        id: "27",
+        image: "public/images/gifts/gift_27.jpg",
+        productUrl: "https://mpago.la/2KPhQsw"
+      },
+      {
+        id: "28",
+        image: "public/images/gifts/gift_28.jpg",
+        productUrl: "https://mpago.la/2KPhQsw"
+      },
+      {
+        id: "29",
+        image: "public/images/gifts/gift_29.jpg",
+        productUrl: "https://mpago.la/1VdgQQg"
+      },
+      {
+        id: "30",
+        image: "public/images/gifts/gift_30.jpg",
+        productUrl: "https://mpago.la/1VdgQQg"
+      },
+      {
+        id: "31",
+        image: "public/images/gifts/gift_31.jpg",
+        productUrl: "https://mpago.la/1JBKkze"
+      },
+      {
+        id: "32",
+        image: "public/images/gifts/gift_32.jpg",
+        productUrl: "https://mpago.la/1AFNEp3"
+      },
+      {
+        id: "33",
+        image: "public/images/gifts/gift_33.jpg",
+        productUrl: "https://mpago.la/1YEygAH"
+      },
+      {
+        id: "34",
+        image: "public/images/gifts/gift_34.jpg",
+        productUrl: "https://mpago.la/1Muvv9f"
+      },
+      {
+        id: "35",
+        image: "public/images/gifts/gift_35.jpg",
+        productUrl: "https://mpago.la/1qwfamvg"
+      },
+      {
+        id: "36",
+        image: "public/images/gifts/gift_36.jpg",
+        productUrl: "https://mpago.la/1FeCN6m"
+      },
+      {
+        id: "37",
+        image: "public/images/gifts/gift_37.jpg",
+        productUrl: "https://mpago.la/2VjPQmn"
+      },
+      {
+        id: "38",
+        image: "public/images/gifts/gift_38.jpg",
+        productUrl: "https://mpago.la/2DLwyCB"
+      },
+      {
+        id: "39",
+        image: "public/images/gifts/gift_39.jpg",
+        productUrl: "https://mpago.la/13MoyhA"
+      },
+      {
+        id: "40",
+        image: "public/images/gifts/gift_40.jpg",
+        productUrl: "https://mpago.la/2dmSyJQ"
+      },
+      {
+        id: "41",
+        image: "public/images/gifts/gift_41.jpg",
+        productUrl: "https://mpago.la/1Gnv2W4"
+      },
+      {
+        id: "42",
+        image: "public/images/gifts/gift_42.jpg",
+        productUrl: "https://mpago.la/17oMbR5"
+      },
+      {
+        id: "43",
+        image: "public/images/gifts/gift_43.jpg",
+        productUrl: "https://mpago.la/25zRpku"
+      },
+      {
+        id: "44",
+        image: "public/images/gifts/gift_44.jpg",
+        productUrl: "https://mpago.la/1nnnXDM"
+      },
+      {
+        id: "45",
+        image: "public/images/gifts/gift_45.jpg",
+        productUrl: "https://mpago.la/2xhhHFK"
+      },
+      {
+        id: "46",
+        image: "public/images/gifts/gift_46.jpg",
+        productUrl: "https://mpago.la/1ao7UZg"
+      },
+      {
+        id: "47",
+        image: "public/images/gifts/gift_47.jpg",
+        productUrl: "https://mpago.la/1ngKByR"
+      },
+      {
+        id: "48",
+        image: "public/images/gifts/gift_48.jpg",
+        productUrl: "https://mpago.la/1LcubDe"
+      },
+      {
+        id: "49",
+        image: "public/images/gifts/gift_49.jpg",
+        productUrl: "https://mpago.la/1jq4wQW"
+      },
+      {
+        id: "50",
+        image: "public/images/gifts/gift_50.jpg",
+        productUrl: "https://mpago.la/1uMPqfW"
+      },
+      {
+        id: "51",
+        image: "public/images/gifts/gift_51.jpg",
+        productUrl: "https://mpago.la/2Vz54EH"
+      },
+      {
+        id: "52",
+        image: "public/images/gifts/gift_52.jpg",
+        productUrl: "https://mpago.la/1F6zrtY"
+      },
+      {
+        id: "53",
+        image: "public/images/gifts/gift_53.jpg",
+        productUrl: "https://mpago.la/1nRuByd"
+      },
+      {
+        id: "54",
+        image: "public/images/gifts/gift_54.jpg",
+        productUrl: "https://mpago.la/1CVhsX9"
+      },
+      {
+        id: "55",
+        image: "public/images/gifts/gift_55.jpg",
+        productUrl: "https://mpago.la/1CVhsX9"
+      },
+      {
+        id: "56",
+        image: "public/images/gifts/gift_56.jpg",
+        productUrl: "https://mpago.la/1uMPqfW"
+      },
+      {
+        id: "57",
+        image: "public/images/gifts/gift_57.jpg",
+        productUrl: "https://mpago.la/1g3MLK9"
+      },
+      {
+        id: "58",
+        image: "public/images/gifts/gift_58.jpg",
+        productUrl: "https://mpago.la/2mjM7p1"
+      },
+      {
+        id: "59",
+        image: "public/images/gifts/gift_59.jpg",
+        productUrl: "https://mpago.la/1XMAonN"
+      },
+      {
+        id: "60",
+        image: "public/images/gifts/gift_60.jpg",
+        productUrl: "https://mpago.la/2NKFEzk"
       }
     ]
   },
