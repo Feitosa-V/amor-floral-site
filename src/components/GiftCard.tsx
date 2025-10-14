7ebb03b56e04affdf9eb7507d2ccc815933899e4
Vinícius Feitosa <vifesi4321@gmail.com>
2025-10-14 00:11:48 -0300

    quase finalizando

--- src/components/GiftCard.tsx
+++ src/components/GiftCard.tsx
@@ -15,7 +15,7 @@ const GiftCard = ({ id, image, onGiveGift }: GiftCardProps) => {
         <img 
           src={image} 
           alt="Presente"
-          className="w-full h-full object-cover transition-transform duration-300 hover:scale-105"
+          className="w-full h-full object-contain  transition-transform duration-300 hover:scale-105"
         />
       </div>
       <div className="p-3">
